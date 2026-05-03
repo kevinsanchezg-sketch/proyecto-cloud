48471851edded53e44fd22756653a44a838bd0d9
Kevin Moises <kevinsanchezg@cun.edu.co>
2026-05-02 20:20:50 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let tasks = [];
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
+app.post('/tasks', (req, res) => {
+    const task = req.body.task;
+    tasks.push(task);
+    res.send("Tarea agregada");
+});
+
+app.delete('/tasks/:index', (req, res) => {
+    const index = req.params.index;
+    tasks.splice(index, 1);
+    res.send("Tarea eliminada");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Servidor corriendo');
+});
